fd403083397ee06b987f1489530571e1d6b244d5
IvanElRincon <franciscoivanramirezortega@alumno.ieselrincon.es>
2023-03-07 11:42:24 +0000

    Pepe el pepe

--- Pantalla/src/main/java/Conexiones/MiConexion.java
+++ Pantalla/src/main/java/Conexiones/MiConexion.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Conexiones;
 
-/**
- *
- * @author suare
- */
-public class MiConexion {
-    private int a;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    static Connection con = null;
+    static String URL = "jdbc:mysql://localhost:3306/";
+    static String USER = "ivan";
+    static String PASSWORD = "ivanPass";
+
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("No se ha encontrado el driver de JDBC " + cnfe);
+        } catch (SQLException sqle) {
+            System.out.println("Error al conectarse con la base de datos: " + sqle);
+        }
+
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
 }
