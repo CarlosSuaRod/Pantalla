d72e5d36ed02ff14c9e19881c805479838d78536
Saulompr99 <57179314+Saulompr99@users.noreply.github.com>
2023-03-10 10:48:39 +0000

    Espera a cargar el contenido

--- html/main.js
+++ html/main.js
@@ -1,11 +1,11 @@
-//
+//Gestion guaguas
 
 function refrescar() {
     document.getElementsByClassName("container-fluid")[0].classList.add("vanish");
     setTimeout(window.location.reload(),1000);
 }
 
-setTimeout(refrescar, 60000);
+//setTimeout(refrescar, 60000);
 
 var tablas = document.getElementsByTagName("tbody");
 
@@ -27,36 +27,31 @@ if(tablas.length<2) {
 }
 
 //Colocar etiquetas tr
-for(var i=0; i<tablas.length; i++) {
-    for(var j=0; j<tablas[i].children.length; j++) {
-        var fun = (tablas,i,j)=>{tablas[i].children[j].classList.add("colocar");};
-        setTimeout(fun, 500*j, tablas, i, j);
+function animacionTablas() {
+    for(var i=0; i<tablas.length; i++) {
+        for(var j=0; j<tablas[i].children.length; j++) {
+            var fun = (tablas,i,j)=>{tablas[i].children[j].classList.add("colocar");};
+            setTimeout(fun, 500*j, tablas, i, j);
+        }
     }
 }
 
+
+//gestion iframes
+
+
+
+var id = setInterval(async function a() {
+    var iframes = document.getElementsByTagName("iframe");
+    if(iframes.length >= 4) {
+        clearInterval(id);
+        
+        document.getElementsByTagName("html")[0].classList.add("unvanish");
+        animacionTablas();
+    }
+}, 100);
 /*
-//Reloj analógico
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
-
- function setDate() {
-  const now = new Date();
-
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-  const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-}
-
-setInterval(setDate, 1000);
-
-setDate();
+myIframe.addEventListener("load", function() {
+  this.contentWindow.document.notesform.ID_client.value = Client;
+});
 */
